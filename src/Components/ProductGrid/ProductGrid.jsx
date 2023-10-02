84d72bb3702f1cf50e3bb3792f4e7af7e47d5133
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-10-02 18:40:13 -0300

    :lipstick: Added ItemListContainer

--- src/Components/ProductGrid/ProductGrid.jsx
+++ src/Components/ProductGrid/ProductGrid.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
 
-const ProductGrid = () => {
+const ProductGrid = (props) => {
   return (
-    <section className="">
+    <section>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <ProductCard />
+            <ProductCard product={props.product} />
           </div>
         </div>
       </div>
