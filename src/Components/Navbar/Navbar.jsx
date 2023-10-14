5e8326702fdc1d0e03453c834d602370e5542884
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-10-13 22:41:04 -0300

    :lipstick: Enhanced page title based on navbar click

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import musicCenterLogo from "../../assets/logos/logo.png";
 import CartWidget from "../CartWidget/CartWidget";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [pageTitle, setPageTitle] = useState("Inicio");
+
+  useEffect(() => {
+    document.title = `${pageTitle} | Music Center`;
+  }, [pageTitle]);
+
+  const handlePageTitle = (pageTitle) => {
+    setPageTitle(pageTitle);
+  };
+
   return (
     <nav className="navbar navbar-custom navbar-expand-md">
       <div className="container-fluid">
-        <a className="navbar-brand" href="index.html">
+        <a
+          onClick={() => setPageTitle("Inicio")}
+          className="navbar-brand"
+          href="index.html"
+        >
           <img src={musicCenterLogo} alt="music-center-logo" />
         </a>
         <button
@@ -37,27 +52,48 @@ const Navbar = () => {
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="index.html">
+                <a
+                  onClick={() => setPageTitle("Inicio")}
+                  className="nav-link"
+                  aria-current="page"
+                  href="index.html"
+                >
                   Inicio
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a
+                  onClick={() => setPageTitle("Guitarras")}
+                  className="nav-link"
+                  href="#"
+                >
                   Guitarras
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a
+                  onClick={() => setPageTitle("Amplificadores")}
+                  className="nav-link"
+                  href="#"
+                >
                   Amplificadores
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a
+                  onClick={() => setPageTitle("Pedales")}
+                  className="nav-link"
+                  href="#"
+                >
                   Pedales
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a
+                  onClick={() => setPageTitle("Accesorios")}
+                  className="nav-link"
+                  href="#"
+                >
                   Accesorios
                 </a>
               </li>
