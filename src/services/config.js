3013b7a90a38e6fabb78612af8978069b628ebc6
Agustin Salvidio <agustinsalvidio@gmail.com>
2023-11-13 09:09:47 -0300

    :closed_lock_with_key: Added .env secret for Firebase API KEY

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  //Needs to be completed
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "music-center-1d0ae.firebaseapp.com",
+  projectId: "music-center-1d0ae",
+  storageBucket: "music-center-1d0ae.appspot.com",
+  messagingSenderId: "598312616206",
+  appId: "1:598312616206:web:0aab4c5ff0b32f380d975f",
 };
 
 const app = initializeApp(firebaseConfig);
